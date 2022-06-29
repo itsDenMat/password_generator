242e655c0d9ee7a8d3b28a6c9b7952389e67a03a
itsDenMat <dennismateo93@gmail.com>
2022-06-28 22:42:50 -0700

    Add variables and array for all characters.

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-// Assignment Code
+// User variables
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
+
+// Password variable values
+specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"]
+number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+aplha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
