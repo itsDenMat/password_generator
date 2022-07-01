99e3e56e1a31ad25ed9e595345ddc773795988ab
itsDenMat <dennismateo93@gmail.com>
2022-06-30 23:39:32 -0700

    update var for uppercase and lowercase letters in js file.

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ var confirmLowercase;
 // Password variable values
 specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"]
 number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-aplha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+UppLet = ["A", "B", "C", "D", "E", "F", "G", "H","I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "W", "X", "Y", "Z"];
+lowLet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
 var generateBtn = document.querySelector("#generate");
@@ -19,7 +20,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
