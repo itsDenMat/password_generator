03e3d59878cccbbc6c2ce1ec4e69ee38e72f2f62
itsDenMat <dennismateo93@gmail.com>
2022-07-04 00:55:52 -0700

    Add if statement to prevent 'undefined' in text box when exiting selection

--- script.js
+++ script.js
@@ -90,7 +90,9 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  if (password) {
   passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
